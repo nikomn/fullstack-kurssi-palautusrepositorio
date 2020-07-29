4d2cf97b169c01d9ca0d2ff578d909584ca0fb78
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-07-29 10:18:07 +0300

    unicafe - Step 3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -12,12 +12,18 @@ import ReactDOM from 'react-dom'
 const handleNeutralClick = () =>
   setClicks({ ...clicks, neutral: clicks.neutral + 1 }) */
 
-const Stats = (props) => {
+const Statistics = (props) => {
   console.log(props)
+  const all = props.good + props.neutral + props.bad
+  const avr = (props.good * 1 + props.neutral * 0 + props.bad * -1) / all
+  const positive = props.good / all * 100
+  
   return (
-    <div>
-      ALL: {props.good + props.neutral + props.bad} 
-    </div>
+    <>
+      <div>ALL: {all}</div>
+      <div>AVARGE: {avr}</div>
+      <div>POSITIVE: {positive} %</div>
+    </>
   )
 }
 
@@ -39,11 +45,12 @@ const App = () => {
       <div>GOOD: {good}</div>
       <div>NEUTRAL: {neutral}</div>
       <div>BAD: {bad}</div>
-      <div>ALL: {(good + neutral + bad)}</div>
+      {/* <div>ALL: {(good + neutral + bad)}</div>
       <div>Avarage: {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}</div>
-      <div>Positive: {good / (good + neutral + bad) * 100} %</div>
+      <div>Positive: {good / (good + neutral + bad) * 100} %</div> */}
     {/* <div>{(good + neutral + bad) / 3}</div> */}
       {/* {Stats("good"={good}, "neutral"={neutral}, "bad"={bad})} */}
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
