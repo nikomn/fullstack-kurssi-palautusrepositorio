d8f31bc3316fef4f4c926e57053b2659f7655647
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-07-30 11:50:29 +0300

    kurssitiedot step7

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -51,10 +51,35 @@ const Course = (props) => {
     <div>
       <Header course={props.course.name} />
       <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
     </div>
 
   )
   
+}
+
+const Total = (props) => {
+  console.log("Total - props:")
+  console.log(props)
+  
+  // ...liian Old School!!!
+  /* var total = 0
+  for (var i = 0; i < props.parts.length; i++) {
+    //console.log(props.parts[i].exercises)
+    total = total + props.parts[i].exercises
+  } */
+
+  var total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+
+
+
+  console.log(total)
+  return (
+    <div>
+      <p><strong>Total number of exercises {total}</strong></p>
+    </div>
+  )
 }
 
 
@@ -81,7 +106,7 @@ const App = () => {
       },
       {
         name: 'State of a component II',
-        exercises: 19,
+        exercises: 11,
         id: 4
       }
     ]
