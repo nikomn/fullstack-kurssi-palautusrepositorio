e273d877945b28a037c60f70fff2e32a87dfd32c
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-02 09:24:39 +0300

    kurssitiedot step9

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 
 
 const Header = (props) => {
-  console.log("Header - props:")
-  console.log(props)
+  //console.log("Header - props:")
+  //console.log(props)
   return (
     <div>
-      <h1>
+      <h2>
         {props.course}
-      </h1>
+      </h2>
     </div>
   )
 }
@@ -59,8 +59,8 @@ const Course = (props) => {
 }
 
 const Total = (props) => {
-  console.log("Total - props:")
-  console.log(props)
+  //console.log("Total - props:")
+  //console.log(props)
   
   // ...liian Old School!!!
   /* var total = 0
@@ -91,36 +91,67 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'State of a component II',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  
+
+  /* courses.forEach(value => {
+    console.log(value)
+  }) */
+
+
+
+
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web Dev Courses</h1>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )}
     </div>
   )
 }
