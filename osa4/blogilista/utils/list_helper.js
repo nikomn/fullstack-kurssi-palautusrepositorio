ed83cb8d8cf5bf4f0e053d9fe9cbb029d7e495e1
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-13 16:57:37 +0300

    apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     return 1
   }
@@ -7,9 +9,60 @@ const totalLikes = (blogs) => {
   
 }
 
+const favouriteBlog = (blogs) => {
+
+    /* var favourite = 
+      {
+        "title": "null",
+        "author": "null",
+        "likes": -9999
+      }
+
+    console.log(favourite) */
+
+    const favourite = blogs.reduce(function(prev, current) {
+      return (prev.likes > current.likes) 
+        ? prev 
+        : current
+    })
+
+    /* const title = favourite.title
+    const author = favourite.author
+    const likes = favourite.likes
+
+    const x = {
+      title,
+      author,
+      likes
+    } */
+
+    return favourite
+    
+
+    /* var favourite = blogs.reduce(function(a, b) {
+
+      return Math.max(a, b);
+    }); */
+    
+    /* for (var i = 0; i < blogs.lenght; i++) {
+      if (blogs[i].likes > favourite.likes) {
+        favourite = blogs[i]
+        console.log(favourite)
+        console.log(blogs[i])
+      }
+    } */
+
+
+  
+
+  
+  
+}
+
 
   
   module.exports = {
     dummy,
     totalLikes,
+    favouriteBlog,
   }
