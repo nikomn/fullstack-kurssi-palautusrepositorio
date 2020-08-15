cce24419f789c65a0e08fb1d03235f3fb694c60e
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-15 11:35:41 +0300

    blogilistan testit, step 1, HTTP GET /api/blogs async/awaitilla

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,37 +1,21 @@
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const mongoose = require('mongoose')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-    {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-    },
-    {
-        _id: "5a422b891b54a676234d17fa",
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-        likes: 10,
-        __v: 0
-    }  
-  ]
+
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
-
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
-})
+    await Blog.deleteMany({})
+  
+    let blogObject = new Blog(helper.initialBlogs[0])
+    await blogObject.save()
+  
+    blogObject = new Blog(helper.initialBlogs[1])
+    await blogObject.save()
+  })
 
 test('correct number of blogs are returned', async () => {
     const response = await api.get('/api/blogs')
