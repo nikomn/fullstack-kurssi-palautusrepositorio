cae2c65b239701d4a27df30b43fe487c323e2959
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-17 16:59:35 +0300

    blogilistan laajennus, step8

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -32,10 +33,24 @@ blogsRouter.get('/', async (request, response) => {
     })
 }) */
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'error: token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   
-  const user = await User.findById(body.userId)
+  //const user = await User.findById(body.userId)
 
   if (body.title === undefined || body.url === undefined) {
     //response.status(400)
