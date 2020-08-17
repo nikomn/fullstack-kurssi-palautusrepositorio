5ad339137107208b082ce84d429144031b1a13de
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-17 16:35:54 +0300

    blogilistan laajennus, step6

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,12 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs.map(blog => blog.toJSON()))
+  
+  /* Blog
     .find({})
     .then(blogs => {
       response.json(blogs.map(blog => blog.toJSON()))
-    })
+    }) */
 })
 
 blogsRouter.get('/', async (request, response) => { 
@@ -28,6 +34,8 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  
+  const user = await User.findById(body.userId)
 
   if (body.title === undefined || body.url === undefined) {
     //response.status(400)
@@ -39,12 +47,15 @@ blogsRouter.post('/', async (request, response) => {
       author: body.author,
       url: body.url,
       likes: body.likes === undefined ? 0 : body.likes,
+      user: user._id
     })
   
   
     //const blog = new Blog(request.body)
   
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.json(savedBlog.toJSON())
   }
 })
