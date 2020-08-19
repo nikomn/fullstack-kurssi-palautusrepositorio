d1eb984e0a0c1b7e4671f6d5166979c9bd0200b2
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-19 13:58:43 +0300

    blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,63 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+//import React from 'react'
+
+const Blog = ({ blog }) => {
+  const [showAllInfo, setShowAllInfo] = useState(false)
+
+  /* const toggleShowInfo = () => {
+    setShowAllInfo(!showAllInfo)
+  } */
+
+  //console.log(blog.title)
+
+  const userData = JSON.stringify(blog.user)
+
+  const userDataJSON = JSON.parse(userData)
+
+  //const userName = userDataJSON.name
+
+  //const userData = JSON.parse(blog.user) 
+
+  //console.log('showAllInfo: ', showAllInfo)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+
+  if (showAllInfo) {
+    return (
+      <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button 
+          onClick={() => setShowAllInfo(!showAllInfo)}>
+          {showAllInfo ? 'hide info' : 'show info' }
+        </button><br />
+        {blog.url} <br />
+        likes {blog.likes} <button>like</button> <br />
+        {userDataJSON.name}
+      </div>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button 
+          onClick={() => setShowAllInfo(!showAllInfo)}>
+          {showAllInfo ? 'hide info' : 'show info' }
+        </button>
+      </div>
+      </div>
+      )
+  }
+  
+}
 
 export default Blog
--- osa5/bloglist-frontend/src/components/NewBlogForm.js
+++ osa5/bloglist-frontend/src/components/NewBlogForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 //import React, {useState} from 'react'
 //import React from 'react'
 
@@ -19,7 +19,6 @@ const NewBlogForm = ({ createBlog }) => {
   const addBlog = (event) => {
     event.preventDefault()
     //blogFormRef.current.toggleVisibility()
-
     createBlog({
       title: newBlogTitle,
       author: newBlogAuthor,
