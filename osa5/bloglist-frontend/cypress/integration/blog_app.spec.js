87eeee9ef6317e392c1798b705c76906b82aa473
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-21 09:34:52 +0300

    blogilistan end to end -testit, step6

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -40,10 +40,16 @@ describe('Blog app', function() {
   
   describe('When logged in', function() {
     beforeEach(function() {
-      cy.get('#username').type('testaaja')
+      /* cy.get('#username').type('testaaja')
       cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
+      cy.get('#login-button').click() */
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'testaaja', password: 'salainen'
+      }).then(response => {
+        localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
     })
+  })
 
     it('A blog can be created', function() {
       cy.contains('new blog').click()
@@ -112,10 +118,107 @@ describe('Blog app', function() {
       cy.contains('show info').click()
       cy.get('html').should('not.contain', '#delete-button')
 
-
-
-
   })
+
+    it('blogs are always shown in order of likes', function() {
+      cy.createBlog({
+        title: 'third',
+        author: 'cypres automation',
+        url: 'cypress',
+        likes: 1
+      })
+
+      cy.createBlog({
+        title: 'first',
+        author: 'cypres automation',
+        url: 'cypress',
+        likes: 5
+      })
+
+      cy.createBlog({
+        title: 'second',
+        author: 'cypres automation',
+        url: 'cypress',
+        likes: 4
+      })
+
+
+
+      /* cy.contains('new blog').click()
+      cy.get('#title').type('a blog created by cypress')
+      cy.get('#author').type('cypress automation')
+      cy.get('#url').type('cypress')
+      cy.contains('create').click()
+      cy.contains('a blog created by cypress cypress automation')
+      
+      cy.wait(5000)
+      cy.contains('new blog').click()
+      cy.get('#title').type('another blog created by cypress')
+      cy.get('#author').type('cypress automation')
+      cy.get('#url').type('cypress')
+      cy.contains('create').click()
+      cy.contains('another blog created by cypress')
+      
+      cy.wait(5000)
+      cy.contains('new blog').click()
+      cy.get('#title').type('yet another blog created by cypress')
+      cy.get('#author').type('cypress automation')
+      cy.get('#url').type('cypress')
+      cy.contains('create').click()
+      cy.contains('yet another blog created by cypress cypress automation') */
+
+
+      cy.get('.blog').then( blogs => {
+        console.log('number of blogs', blogs.length)
+        //cy.wrap(buttons[0]).click()
+        cy.wrap(blogs[0]).contains('show info').click()
+        //cy.get('#like-button').click()
+        cy.wrap(blogs[1]).contains('show info').click()
+        //cy.get('#like-button').click()
+        cy.wrap(blogs[2]).contains('show info').click()
+        //cy.get('#like-button').click()
+        cy.wrap(blogs[0]).contains('first')
+        cy.wrap(blogs[0]).contains('likes 5')
+        cy.wrap(blogs[1]).contains('second')
+        cy.wrap(blogs[1]).contains('likes 4')
+        cy.wrap(blogs[2]).contains('third')
+        cy.wrap(blogs[2]).contains('likes 1')
+
+      })
+
+      cy.contains('new blog').click()
+      cy.get('#title').type('new blog')
+      cy.get('#author').type('cypress automation')
+      cy.get('#url').type('cypress')
+      cy.contains('create').click()
+      cy.contains('new blog')
+      cy.wait(5000)
+
+      cy.get('.blog').then( blogs => {
+        cy.wrap(blogs[3]).contains('new blog')
+        cy.wrap(blogs[3]).contains('show info').click()
+        cy.wrap(blogs[3]).contains('likes 0').click()
+        cy.wrap(blogs[3]).contains('like').click()
+      })
+
+      cy.get('.blog').then( blogs => {
+        cy.wrap(blogs[3]).contains('new blog')
+        cy.wrap(blogs[3]).contains('show info').click()
+        cy.wrap(blogs[3]).contains('likes 1').click()
+        cy.wrap(blogs[3]).contains('like').click()
+      })
+
+      cy.get('.blog').then( blogs => {
+        cy.wrap(blogs[3]).contains('third')
+        cy.wrap(blogs[3]).contains('show info').click()
+        cy.wrap(blogs[3]).contains('likes 1').click()
+        cy.wrap(blogs[2]).contains('new blog')
+        cy.wrap(blogs[2]).contains('show info').click()
+        cy.wrap(blogs[2]).contains('likes 2').click()
+      })
+
+
+    })
 
 
   })
