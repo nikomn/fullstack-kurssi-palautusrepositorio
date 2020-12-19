b8ce8148f3df62586f13c19ab424282b54cc86c2
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-12-19 13:22:28 +0200

    9.9: Patientor backend, step2

--- osa9/patientor-backend/index.ts
+++ osa9/patientor-backend/index.ts
@@ -2,9 +2,12 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+import cors from 'cors';
+app.use(cors());
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
